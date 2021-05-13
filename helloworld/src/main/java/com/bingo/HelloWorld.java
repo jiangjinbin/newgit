baac0ea8e7f69007481fe593c4ff63a9ae9c00aa
jiangjinbin <31570330+jiangjinbin@users.noreply.github.com>
2021-05-13 23:19:27 +0800

    33333333

--- helloworld/src/main/java/com/bingo/HelloWorld.java
+++ helloworld/src/main/java/com/bingo/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("11111111111");
         System.out.println("22222222222");
+        System.out.println("33333333333");
     }
 
 }
