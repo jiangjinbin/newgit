dc16bd2a7b8a1bbd1ecf2d38ed8a1a26af2a3c7c
bingo <1696875898@qq.com>
2021-05-13 23:22:17 +0800

    44444444444

--- helloworld/src/main/java/com/bingo/HelloWorld.java
+++ helloworld/src/main/java/com/bingo/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
         System.out.println("11111111111");
         System.out.println("22222222222");
         System.out.println("33333333333");
+        System.out.println("44444444444");
     }
 
 }
